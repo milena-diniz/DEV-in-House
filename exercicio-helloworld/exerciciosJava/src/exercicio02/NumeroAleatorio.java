590ce35b7ea91bbf54b441ed8bb82914824d6e72
Milena Diniz Vieira <miladvieira@outlook.com>
2021-03-23 12:51:58 -0300

    organizando exercicios

---
+++ exercicio-helloworld/exerciciosJava/src/exercicio02/NumeroAleatorio.java
@@ -0,0 +1,12 @@
+package exercicio02;
+
+public class NumeroAleatorio {
+
+	public static void main(String[] args) {
+		
+		
+		
+
+	}
+
+}
---
+++ exercicio-numerosAleatorios/numeroAleatorio/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Random aleatorio = new Random();
+        int valor = aleatorio.nextInt(2);
+        System.out.println("Número gerado: " + valor);
+    }
+}
