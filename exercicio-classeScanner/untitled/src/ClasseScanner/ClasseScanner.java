a42b5c7c1d106685bbdd33304f957485d8e4d882
Milena Diniz Vieira <miladvieira@outlook.com>
2021-03-25 20:30:10 -0300

    exercicio 7

---
+++ exercicio-classeScanner/untitled/src/ClasseScanner/ClasseScanner.java
@@ -0,0 +1,16 @@
+package ClasseScanner;
+
+import java.util.Scanner;
+
+public class ClasseScanner {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        System.out.printf("Informe seu sobrenome: ");
+        String sobrenome = ler.next();
+        System.out.printf("Informe seu nome: ");
+        String nome = ler.next();
+        System.out.println("Seu nome completoddddd é " + nome + " " +sobrenome) ;
+
+
+    }
+}
