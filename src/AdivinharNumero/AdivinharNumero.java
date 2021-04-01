d8f7063a9500c6fe84e9ed17e1cc1466714240fe
Milena Diniz Vieira <miladvieira@outlook.com>
2021-03-31 21:30:10 -0300

    Exercício

--- src/AdivinharNumero/AdivinharNumero.java
+++ src/AdivinharNumero/AdivinharNumero.java
@@ -1,2 +1,33 @@
-package AdivinharNumero;public class AdivinharNumero {
+package AdivinharNumero;
+
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class AdivinharNumero {
+    public static void main (String[] args) {
+        Random random = new Random();
+        int numeroRandomico = random.nextInt(6);
+        Scanner ler = new Scanner(System.in);
+
+        System.out.printf("Adivinhe qual número de 1 a 5 eu estou pensando. Escreva um número aqui de 1 a 5: ");
+        int resposta = ler.nextInt();
+        if (resposta == numeroRandomico) {
+            System.out.printf("Sucesso! Você acertou, parabéns!");
+        } else if (resposta > 5 || resposta < 1) {
+            System.out.printf("Erro! Você deve informar um número de 1 a 5.");
+        } else {
+            System.out.printf("Erro! O número correto é: " + numeroRandomico );
+        }
+
+
+
+
+
+
+
+
+
+
+    }
 }
--- src/TamanhoDeLetras/TamanhoDeLetras.java
+++ src/TamanhoDeLetras/TamanhoDeLetras.java
@@ -1,2 +1,18 @@
-package TamanhoDeLetras;public class TamanhoDeLetras {
+package TamanhoDeLetras;
+
+import java.util.Scanner;
+import java.lang.String;
+
+public class TamanhoDeLetras{
+
+    public static void main(String[] args){
+        Scanner ler = new Scanner(System.in);
+        System.out.printf("Digite um nome qualquer: ");
+        String exampleString = ler.next();
+        int stringLength = exampleString.length();
+        System.out.println("String length: " + stringLength);
+
+    }
+
+
 }
